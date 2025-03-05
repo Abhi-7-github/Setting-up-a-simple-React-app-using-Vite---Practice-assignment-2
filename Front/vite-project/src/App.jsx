edb0fb6256e96f3404e0a74d195578a6725804ef
Abhiram <abhikollepara333@gmail.com>
2025-03-05 14:58:17 +0530

    Done

---
+++ Front/vite-project/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { motion } from "motion/react"
+import './App.css'; 
+
+
+
+const App = () => {
+  return (
+     <>
+       <motion.div  
+       className='box'
+       
+       
+       >
+        <div className='div'>
+        <h1 className='Name'>Abhiram Kollepara</h1>
+        <h2 className='Email'>✉️abhikollepara33333@gmail.co.in</h2>
+        </div>
+        
+
+
+       </motion.div>
+     
+     </>
+  )
+}
+
+export default App
